fc0598143ccaeff12776a778c5f1b17db1d1c6c5
rylda <71103413+rylda@users.noreply.github.com>
2020-11-26 13:42:34 -0600

    enjoy

--- classportfolio/modal_pop/main.js
+++ classportfolio/modal_pop/main.js
@@ -1,26 +1,17 @@
-function deselect(e) {
-  $('.pop').slideFadeToggle(function() {
-    e.removeClass('selected');
+function deselect(e) {  $('.pop').slideFadeToggle(function() {
+    e.removeClass('active') ();
   });    
 }
-
-$(function() {
-  $('#intro').on('click', function() {
+$(function() { $('#intro').on('click', function() {
     if($(this).hasClass('selected')) {
-      deselect($(this));               
-    } else {
-      $(this).addClass('selected');
-      $('.pop').slideFadeToggle();
-    }
-    return false;
-  });
+      deselect($(this));                } else {
+      $(this).addClass('selected');  $('.pop').slideFadeToggle();
+    } return false;  });
 
-  $('.close').on('click', function() {
-    deselect($('#contact'));
-    return false;
-  });
+  $('.close').on('click', function() { deselect($('#intro'));
+    return false;});
 });
 
-$.fn.slideFadeToggle = function(easing, callback) {
-  return this.animate({ opacity: 'toggle', height: 'toggle' }, 'fast', easing, callback);
+$.fn.slideFadeToggle = function(easing, callback) { return this.animate({ 
+    opacity: 'toggle', height: 'toggle' }, 'fast', easing, callback);
 };
